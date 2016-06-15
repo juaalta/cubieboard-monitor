1aa57d96b555e5b3bc8989042d3896283b55d27e
juaalta <juaalta@gmail.com>
2016-06-15 02:43:03 +0200

    Actualizados datos del fichero package.json. Añadido fichero de log al gitignore. Añadidos nuevos servicios:    - Apagado de la máquina.    - Información del uso de cada uno de los cores de la CPU. Documentado el fichero server.js Actualizadas referencias de ficheros de CPU y HDD a sus js correspondientes.

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var util = require('util')
 var exec = require('child_process').exec;
 var child;
 
-// Configuración
-app.configure(function() {
+var logger = require('express-logger');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+/*
+ * Configuración
+ */
+
+/*app.configure(function() {
     // Localización de los ficheros estÃ¡ticos
     app.use(express.static(__dirname + '/'));
     // Muestra un log de todos los request en la consola
@@ -15,11 +22,25 @@ app.configure(function() {
     app.use(express.bodyParser());
     // Simula DELETE y PUT
     app.use(express.methodOverride());
-});
+});*/
 
+//File localization
+app.use(express.static(__dirname + '/'));
+// Show a log of every request in the terminal console
+app.use(logger({
+    path: "logfile.txt"
+}));
+//Change html
+app.use(bodyParser());
+//For delete and put API methods
+app.use(methodOverride());
 
+/*
+ * Servicios Rest
+ */
+
+//Obtención de la temperatura general de la CPU
 app.get('/CPUTemp', function(req, res) {
-    // executes `pwd`
     child = exec("./scripts/temperatura.sh", function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
@@ -30,8 +51,8 @@ app.get('/CPUTemp', function(req, res) {
     });
 })
 
+//Obtención del uso general de la CPU
 app.get('/varCPU', function(req, res) {
-    // executes `pwd`
     child = exec("./scripts/var_CPU.sh", function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
@@ -42,8 +63,53 @@ app.get('/varCPU', function(req, res) {
     });
 })
 
+//Obtención del uso general de la CPU
+app.get('/varCPUCores', function(req, res) {
+    var result = []
+    child = exec("./scripts/var_CPU_id.sh 0", function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        result.push({
+            core: 0,
+            var: stdout
+        });
+    });
+
+    child = exec("./scripts/var_CPU_id.sh 1", function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        result.push({
+            core: 1,
+            var: stdout
+        });
+    });
+
+    res.contentType('application/json');
+    console.log(result);
+    res.send(JSON.stringify(result));
+})
+
+//Obtención de la temperatura del HDD
+app.get('/CPUTemp', function(req, res) {
+    child = exec("sudo hddtemp /dev/sda | awk '{print $4d}'", function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        res.end(stdout)
+    });
+})
+
+
+//Obtención del nombre de la máquina.
 app.get('/hostname', function(req, res) {
-    // executes `pwd`
     child = exec("hostname", function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
@@ -54,12 +120,26 @@ app.get('/hostname', function(req, res) {
     });
 })
 
+//Apagado de la máquina.
+app.get('/shutdown', function() {
+    child = exec("sudo shutdown -h 0", function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        res.end(stdout)
+    });
+
+})
+
 // Carga una vista HTML simple donde irá nuestra Single App Page
 // Angular Manejará el Frontend
 app.get('*', function(req, res) {
     res.sendfile('./index.html');
 });
 
+//Configuración del puerto en el que se serviran los servicios web.
 var server = app.listen(8081, function() {
 
     var host = server.address().address
