193d86c5b42aa9a792b6bf637bca0e19d4671b82
juaalta <juaalta@gmail.com>
2017-02-06 23:41:22 +0100

    Actualizados scripts de temperatura de CPU.

--- scripts/CPU_Temp.sh
+++ scripts/CPU_Temp.sh
@@ -2,3 +2,4 @@
 
 #cat /sys/devices/platform/coretemp.0/hwmon/hwmon0/temp1_input | awk '{ printf ("%0.1f\n",$1/1000); }'
 sensors | grep "temp1" | awk '{ printf ("%1.2f\n",$2);}'
+cat /sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input | awk '{ printf ("%0.1f\n",$1/1000); }'
